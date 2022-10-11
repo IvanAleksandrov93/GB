02dd36003b51a3e59dd8af727ce7008f4f7bd27e
Ivan <ivanalexandrov93@yandex.ru>
2022-10-11 21:23:28 +0500

    добавили еще папку в игнор

--- С#/Example002_hellouser/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿int numberA = 32;
-int numberB = 5;
-Console.WriteLine(numberA+numberB);
--- С#/Seminars/Seminar1/Task8/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int n = Convert.ToInt32(Console.ReadLine());
-    if (n % 2 == 0)
-    {
-        Console.WriteLine("число: " + n + " четное");
-        }
-        else {
-            Console.WriteLine("число: " + n + " не четное");
-             }
