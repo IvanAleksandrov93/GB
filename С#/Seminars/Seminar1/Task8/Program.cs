f055cd8fb40f5203a794b88fffdd36248a640607
Ivan <ivanalexandrov93@yandex.ru>
2022-10-11 23:35:47 +0500

    наконеец то решил задание 8

---
+++ С#/Seminars/Seminar1/Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int n = 1;
+int m = Convert.ToInt32(Console.ReadLine());
+       for (int i=n; i<=m; i++)
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
