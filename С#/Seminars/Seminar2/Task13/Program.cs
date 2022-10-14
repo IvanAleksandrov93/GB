ee3c435909f8cc8aef9c854774549dfa211ae0c2
Ivan <ivanalexandrov93@yandex.ru>
2022-10-14 20:11:17 +0500

    выполнил задание 13

---
+++ С#/Seminars/Seminar2/Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string str = n.ToString();
+Console.WriteLine(n);
+if (n<100)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine(str[2]);
