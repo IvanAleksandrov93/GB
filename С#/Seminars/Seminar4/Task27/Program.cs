9e55dd268d356e916abed72ffc1e2941dcd92339
Ivan <ivanalexandrov93@yandex.ru>
2022-10-19 03:11:36 +0500

    Закончил задание 27

---
+++ С#/Seminars/Seminar4/Task27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int length = number.Length;
+int res = 0;
+for (int i = 0; i < length; i++ )
+{
+    res = res + (int)Char.GetNumericValue(number[ i ]);
+}
+Console.WriteLine(res);
