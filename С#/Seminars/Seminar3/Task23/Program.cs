6f7d3ea41b828c74e6b80c9b10f4a01f33c257b1
Ivan <ivanalexandrov93@yandex.ru>
2022-10-19 01:56:45 +0500

    наконец то закончил задание 23

---
+++ С#/Seminars/Seminar3/Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int n = 0;
+int m = Convert.ToInt32(Console.ReadLine());
+       for (int i=1; i<=m; i++)
+     {
+       n= (int)Math.Pow(i,3);
+       if (i==m) Console.Write(n + " ");
+       else 
+        Console.Write(n + " ");
+     }
+
+
+
+    
+
+
+
+
+
+     //string str = n.ToString();
