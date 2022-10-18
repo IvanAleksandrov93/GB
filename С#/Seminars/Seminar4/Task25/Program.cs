ef128abfd3e35b648136d1da9976577dfaf6e6aa
Ivan <ivanalexandrov93@yandex.ru>
2022-10-19 02:10:23 +0500

    Завершил задание 25

---
+++ С#/Seminars/Seminar4/Task25/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень для числа");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Pow(n,m));
