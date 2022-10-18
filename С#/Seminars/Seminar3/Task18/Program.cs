30ca76d48c3754172603065d2d131066aa9a0fb2
Ivan <ivanalexandrov93@yandex.ru>
2022-10-18 23:42:10 +0500

    выполнил задание 18

---
+++ С#/Seminars/Seminar3/Task18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = n.ToString();
+
+if (str[0]==str[4] && str[1]==str[3])
+    { 
+        Console.WriteLine ($"полиндром");
+    }
+else
+    {
+    Console.WriteLine($"не полиндром");
+    }
+
+
+
