1d6e568fde2693810fc925b69f3b07d49835110a
Ivan <ivanalexandrov93@yandex.ru>
2022-10-25 21:26:56 +0500

    задание №38 выполнено

---
+++ С#/Seminars/Seminar5/Task38/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+
+FillArray(array);
+
+PrintArray(array);
+Console.WriteLine();
+
+Rasnitsa(array);
+
+void FillArray (double [] array)   
+{
+    Random rnd = new Random();
+    for (int i =0; i<array.Length; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble()*100, 0);
+        }
+}
+
+void PrintArray(double[] massive1)
+{
+    for (int j=0; j<n; j++)
+    Console.Write($"{massive1[j]}" + " ");
+}
+
+void Rasnitsa (double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i=0; i<n; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    double raz = max - min;
+    Console.Write($"{max}" + " ");
+    Console.Write($"{min}");
+    Console.WriteLine();
+    Console.Write($"Разница между max и min: {raz}");
+}
