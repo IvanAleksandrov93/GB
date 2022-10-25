dd6702b5056082ed6dcae22d7a9c0ee84ad7f0c2
Ivan <ivanalexandrov93@yandex.ru>
2022-10-25 20:00:18 +0500

    задание №34 выполнено

--- С#/Seminars/Seminar1/Task0/Program.cs
+++ С#/Seminars/Seminar1/Task0/Program.cs
@@ -1,12 +1,36 @@
-﻿Console.WriteLine("введите первое число");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите второе число");
-int m = Convert.ToInt32(Console.ReadLine());
-if (n== m*m )
+﻿// Console.WriteLine("введите первое число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n== m*m )
+// {
+//     Console.WriteLine("да");
+// }
+// else 
+// {
+//     Console.WriteLine("нет");
+// }
+  
+
+  
+int [] array = new int [10];
+Console.Write("Ваш Массив: ");
+for( int i =0; i< array.Length; i++)
 {
-    Console.WriteLine("да");
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
 }
-else 
+
+for( int i =0; i< array.Length /2  ; i++)
 {
-    Console.WriteLine("нет");
+    int t = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length -i - 1] = t;
+}
+
+Console.WriteLine();
+Console.Write("Перевернутый массив: ");
+for (int i=0; i< array.Length; i++)
+{
+    Console.Write(array[i] + " ");
 }
---
+++ С#/Seminars/Seminar5/Task34/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Chetnii(array);
+
+void FillArray(int[] massive)
+{
+    for (int i =0; i<n; i++)
+    {massive[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray(int[] massive1)
+{
+    for (int j=0; j<n; j++)
+    Console.Write($"{massive1[j]}" + " ");
+   
+}
+void Chetnii(int[] array)
+{
+    int  k= 0;
+    for( int i=0; i<n; i++)
+    {
+        if (array[i] %2 ==0)
+        {
+            k++;
+        }
+    }
+    Console.Write($"Количество четных чисел: {k}");
+}
