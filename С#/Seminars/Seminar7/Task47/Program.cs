53af82e2c30337da079a57318c7d817e539eebc0
Ivan <ivanalexandrov93@yandex.ru>
2022-10-27 21:31:42 +0500

    задание 47 выполнено

---
+++ С#/Seminars/Seminar7/Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double [m,n];
+Random rnd = new Random();
+
+void FillArray (double [,] array)   
+{
+    for (int i =0; i<m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = Convert.ToDouble(rnd.Next(0, 100)/10.0);
+        }        
+    }
+}
+
+void PrintArray(double[,] massive1)
+{
+    { 
+        for (int i = 0; i < m; i++)
+            {
+            for (int j=0; j<n; j++)
+                Console.Write($"{massive1[i ,j]}" + " ");
+                Console.WriteLine();
+            }
+    }
+}
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
