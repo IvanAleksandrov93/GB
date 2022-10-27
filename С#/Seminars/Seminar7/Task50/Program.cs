1c991451bf91300117ea3cb963ef029ab3e56fa6
Ivan <ivanalexandrov93@yandex.ru>
2022-10-28 00:59:49 +0500

    задание №50 выполнено

---
+++ С#/Seminars/Seminar7/Task50/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(1,10));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t");
+        Console.WriteLine();    
+}
+Console.WriteLine();
+Console.WriteLine("Введите координату №1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату №2");
+int b = Convert.ToInt32(Console.ReadLine());
+    if (a>m)
+        Console.WriteLine("нет, не угадал, такого числа");
+    if (b>n)
+        Console.WriteLine("нет, не угадал, такого числа");
+    else
+{
+    Object c = array.GetValue(a,b);
+    Console.WriteLine($"{c}" + "!");
+}
