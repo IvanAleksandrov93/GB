d050af1782f168cbb7c43009f4a7b3e919efbe85
Ivan <ivanalexandrov93@yandex.ru>
2022-11-18 18:48:18 +0500

    задание 64 выполнено

---
+++ С#/Seminars/Seminar9/Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите N");
+int number = Convert.ToInt32(Console.ReadLine());
+Faclorial(number);
+void Faclorial(int n)
+{
+
+    if (n == 1)
+    {
+        Console.Write(n);
+    }
+    else
+    {
+        Console.Write(n);
+        Faclorial(n - 1);
+    }
+}
