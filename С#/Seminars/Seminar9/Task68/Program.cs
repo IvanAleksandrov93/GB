232869d33a105c4e05f0cc14ef1f333febf22ef8
Ivan <ivanalexandrov93@yandex.ru>
2022-11-18 20:51:33 +0500

    Задание 68 выполнено

---
+++ С#/Seminars/Seminar9/Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Значение {Ackermann(m, n)}");
+
+int Ackermann(int m, int n) {
+
+    if(m == 0) {
+        return n + 1;  
+    }
+    if(m > 0 && n == 0) {
+        return Ackermann(m - 1, 1);
+    }
+    if(m > 0 && n > 0) {
+       return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    return Ackermann(m, n);
+}
