7bd2238ebda558b02ce2cf2388b4387f798fca03
Ivan <ivanalexandrov93@yandex.ru>
2022-11-18 20:27:22 +0500

    Задание 66 выполнено

---
+++ С#/Seminars/Seminar9/Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+numbers(m, n, 0);
+
+void numbers(int number1, int number2, int sum) {
+
+    if (number1 > number2) 
+    {
+        Console.WriteLine($"Сумма {sum}"); 
+        return;
+    }
+    sum += (number1 ++);
+    numbers(number1, number2, sum);
+}
